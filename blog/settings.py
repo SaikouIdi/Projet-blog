9ef56e5f8a7cea754254987008dfd1facda0f17e
Saikou Oumarou Diallo <113625080+SaikouIdi@users.noreply.github.com>
2024-09-12 18:32:34 +0000

    config de mysql

--- blog/settings.py
+++ blog/settings.py
@@ -85,9 +85,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'Saikou$default',
+         'USER': 'Saikou',
+         'PASSWORD': 'TechLeader1000@',
+         'HOST': 'Saikou.mysql.pythonanywhere-services.com',       
+         'PORT': '3306',
     }
 }
 
